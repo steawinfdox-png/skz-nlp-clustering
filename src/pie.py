4a5f73543dc1b4bc30d68c49e92fe16cc06a8e13
steawinfdox-png <steawinfdox@gmail.com>
2025-12-10 16:18:42 -0600

    Change plot type from bar to pie chart

--- src/pie.py
+++ src/pie.py
@@ -1,6 +1,6 @@
 import matplotlib as plt
 #plot numerical frequency of all clusters
 plt.figure(figsize=(7,5))
-df["Emotional Scale"].value_counts().plot(kind="bar")
+df["Emotional Scale"].value_counts().plot(kind="pie")
 plt.title("Stray Kids Songs by Predominant Emotion")
 plt.show
